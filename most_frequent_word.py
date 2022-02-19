7bd7f53037973d1df6b980bf55e87fd9272f6127
 vkorey <vvkorey@gmail.com>
2022-02-19 14:06:29 +0300

    removed unnecessary counter and added edge case check

--- most_frequent_word.py
+++ most_frequent_word.py
@@ -4,17 +4,18 @@ def most_frequent_word(text):
     'i'
     >>> most_frequent_word("be the change that you wish to see in the world")
     'the'
+    >>> most_frequent_word("")
+    ''
     """
-    count = 0
+    if text == '':
+        return ''
     most_frequent = 1
     text_list = text.split(' ')
     for word in text_list:
         word_count = text.count(word)
         if most_frequent < word_count:
-            result = count
-            most_frequent = word_count
-        count += 1
-    return text_list[result]
+            result = word
+    return result
 
 
 if __name__ == '__main__':
